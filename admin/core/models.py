ab9268453b5778d8f6cf94ef5db53c962a3485d1
Leonardo Freitas <leofreitas.engineer@gmail.com>
2024-10-25 16:52:47 -0300

    feat: config nginx

--- admin/core/models.py
+++ admin/core/models.py
@@ -1,19 +1,31 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.utils import timezone
+import hashlib
+import time
+import os
+
+def random_filename(instance, filename):
+    # Extrai a extensão do arquivo
+    ext = filename.split('.')[-1]
+    
+    # Usa o timestamp atual e o nome original para gerar o hash simples
+    hash_object = hashlib.md5(f"{filename}{time.time()}".encode('utf-8'))
+    return f"{hash_object.hexdigest()}.{ext}"
+
 
 # Create your models here.
 class Video(models.Model):
     title = models.CharField(max_length=100, unique=True, verbose_name='Título')
     description = models.TextField(verbose_name='Descrição')
-    thumbnail = models.ImageField(upload_to='thumbnails/', verbose_name='Thumbnail')
+    thumbnail = models.ImageField(upload_to=random_filename, verbose_name='Thumbnail')
     slug = models.SlugField(unique=True)
     published_at = models.DateTimeField(verbose_name='Publicado em', null=True, editable=False)
     is_published = models.BooleanField(default=False, verbose_name='Publicado')
     num_likes = models.IntegerField(default=0, verbose_name='Likes', editable=False)
     num_views = models.IntegerField(default=0, verbose_name='Visualizações', editable=False)
     tags = models.ManyToManyField('Tag', verbose_name='Tags', related_name='videos')
-    author = models.ForeignKey('auth.User', on_delete=models.PROTECT, verbose_name='Autor', related_name='videos', editable=False, null=True)
+    author = models.ForeignKey('auth.User', on_delete=models.PROTECT, verbose_name='Autor', related_name='videos', editable=False)
 
     def save(
         self,
